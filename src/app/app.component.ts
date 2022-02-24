da443ce9b6751bd78ca5a2e178998d026d70491e
ErikBaksay <79666150+ErikBaksay@users.noreply.github.com>
2022-02-25 00:12:37 +0100

    Mobile Support Fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,3 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'FlashCards';
 }
-const appHeight = () => {
-  const doc = document.documentElement
-  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
-}
-window.addEventListener('resize', appHeight)
-appHeight()
