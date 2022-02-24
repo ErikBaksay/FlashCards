d1a582f68c88ee9a069d4392fea888d81fdac34d
ErikBaksay <79666150+ErikBaksay@users.noreply.github.com>
2022-02-24 22:18:43 +0100

    New Question System

--- src/app/components/current/current.component.ts
+++ src/app/components/current/current.component.ts
@@ -11,17 +11,43 @@ export class CurrentComponent implements OnInit {
 
   set_data = set_data
   current_set = set_data[0]
+  current_question : number = 0
+  completed : number[] = []
+  isCompleted : boolean = false
 
   constructor(private route: ActivatedRoute) { }
+
+  generateQuestion(){
+    if (this.completed.length == this.current_set.questions.length){
+      this.isCompleted = true
+      return
+    }
+    else{
+      this.isCompleted = false
+      let questionID : number = Math.round(Math.random() * 
+        (this.current_set.questions.length-1))
+      while (this.completed.includes(questionID)){
+        questionID = Math.round(Math.random() * 
+          (this.current_set.questions.length-1))
+      }
+      this.current_question = questionID
+    } 
+  }
+  questionPassed(){
+    this.completed.push(this.current_question)
+    this.generateQuestion()
+  }
+  questionFailed(){
+    this.generateQuestion()
+  }
 
   ngOnInit(): void {
     let setID = parseInt(this.route.snapshot.paramMap.get("set")!)
     for(let i = 0; i < set_data.length; i++){
       if (set_data[i].id == setID){
         this.current_set = set_data[i]
-        return
-      }
-    }
+      }  
+    } 
+    this.generateQuestion()
   }
-
 }
--- src/app/sets-data.ts
+++ src/app/sets-data.ts
@@ -2,19 +2,34 @@ export var set_data = [
     {
         id: 1,
         name: 'Flags',
-        q1 : 'flag1',
-        a1 : 'answer1'
+        questions : [
+            ['flag1','answer1'],
+            ['flag2','answer2'],
+            ['flag3','answer3'],
+            ['flag4','answer4'],
+            ['flag5','answer5'],
+        ]
     },
     {
         id: 2,
         name: 'US States',
-        q1 : 'usstates1',
-        a1 : 'answer1'
+        questions : [
+            ['ustates1','answer1'],
+            ['ustates2','answer2'],
+            ['ustates3','answer3'],
+            ['ustates4','answer4'],
+            ['ustates5','answer5'],
+        ]
     },
     {
         id: 3,
         name: 'Capitals',
-        q1 : 'capitals1',
-        a1 : 'answer1'
+        questions : [
+            ['capitals1','answer1'],
+            ['capitals2','answer2'],
+            ['capitals3','answer3'],
+            ['capitals4','answer4'],
+            ['capitals5','answer5'],
+        ]
     }
 ]
